540a9c0253130f0ca4129397f97beb4687f94b97
VICTOR KEMBOI <vickemtoo97@gmail.com>
2024-07-08 12:58:11 +0300

    restful-code-along

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.types import DECIMAL
 
 db = SQLAlchemy()
 
@@ -7,3 +8,12 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(DECIMAL(db.Decimal))
+    
+    new_column = db.Column(db.String)
+    db.create_all()
+    def __repr__(self):
+        return f'<Plant {self.name} | Price: {self.price}>'
+    
